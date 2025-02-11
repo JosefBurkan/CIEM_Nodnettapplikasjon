d0ac02ef6e34d6138cbf63b0a76c4cb4167029d2
Josef <Josef_burkan22@outlook.com>
2025-02-11 13:06:33 +0100

    Gjør klar klassene etter ER-diagrammet

--- CIEM_Nodnettapplikasjon.Server/Models/UserModel.cs
+++ CIEM_Nodnettapplikasjon.Server/Models/UserModel.cs
@@ -1,6 +1,16 @@
-﻿namespace CIEM_Nodnettapplikasjon.Server.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CIEM_Nodnettapplikasjon.Server.Models
 {
     public class UserModel
     {
+        public int userID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+
     }
 }
--- CIEM_Nodnettapplikasjon.Server/Services/IUserService.cs
+++ CIEM_Nodnettapplikasjon.Server/Services/IUserService.cs
@@ -2,5 +2,13 @@
 {
     public interface IUserService
     {
+        void Login(string email, string password);
+        void PasswordVerification(int userID,  string password);
+        void GenerateSessionToken(int userID);
+
+        void Logout(int userID);
+
+
+
     }
 }
