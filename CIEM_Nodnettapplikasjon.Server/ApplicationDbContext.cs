3ae4a283a6b33e1419cd0c76d33b2d4a0834916d
Befalen <joakimnormann@yahoo.com>
2025-02-16 19:50:52 +0100

    dbcontext

---
+++ CIEM_Nodnettapplikasjon.Server/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Add DbSets for tables here (Example: Users table)
+    public DbSet<User> Users { get; set; }
