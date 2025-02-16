8f6aebb2d0729a205869d4a73a3582422b8de8f1
Befalen <joakimnormann@yahoo.com>
2025-02-16 21:14:57 +0100

    changes

--- CIEM_Nodnettapplikasjon.Server/Models/UserModel.cs
+++ CIEM_Nodnettapplikasjon.Server/Models/UserModel.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations; // For key attribute
+using Microsoft.AspNetCore.Identity; // Identity features
+
 
 namespace CIEM_Nodnettapplikasjon.Server.Models
 {
     public class UserModel
     {
+        [Key] // userID primary key
         public int userID { get; set; }
+
+
         public string name { get; set; }
         public string surname { get; set; }
         public string email { get; set; }
