81639a328872adccf8b917bdb146b3cdb18977f5
Befalen <joakimnormann@yahoo.com>
2025-02-19 12:59:30 +0100

    lagt til metodene

--- CIEM_Nodnettapplikasjon.Server/Repositories/IUserInterface.cs
+++ CIEM_Nodnettapplikasjon.Server/Repositories/IUserInterface.cs
@@ -2,5 +2,13 @@
 {
     public interface IUserInterface
     {
+        Task AddUser(User user);
+
+        Task<User> ModifyUser(int brukerID, User user);
+
+        Task<bool> DeleteUser(int brukerID);
+
+        Task<User> ViewUser(int brukerID);
+
     }
 }
