bbb85b9adfd3acff6fa7c77e8181f07c2c904140
Befalen <joakimnormann@yahoo.com>
2025-02-20 22:01:26 +0100

    Navn endring fra IUserInterface til IUserRepository

---
+++ CIEM_Nodnettapplikasjon.Server/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using CIEM_Nodnettapplikasjon.Server.Models;
+
+namespace CIEM_Nodnettapplikasjon.Server.Repositories
+
+{
+    public interface IUserRepository
+    {
+        void AddUser(string username, string email, string phone, string password, string role);
+        void ModifyUser(int userID, string newUsername, string newEmail, string newPhone, string newRole);
+        void DeleteUser(int userID);
+        UserModel ViewUser(int userID);
+    }
+}
