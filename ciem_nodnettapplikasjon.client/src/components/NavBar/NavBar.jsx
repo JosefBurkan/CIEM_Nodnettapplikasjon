f95e0ed053557df91870b1620242d306f4493ecd
Erik Nygård <erikn@uia.no>
2025-02-25 19:14:06 +0100

    Flyttet frontend fra personlig repo til hovedprosjekt

---
+++ ciem_nodnettapplikasjon.client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import styles from './NavBar.module.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+function NavBar(){
+
+    return(
+        <h1>Test</h1>
+    );
+
+}
+
+export default NavBar;
