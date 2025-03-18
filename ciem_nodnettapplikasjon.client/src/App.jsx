8753c606fd2230b212df9d7f8ed79c6491d4387e
Ajdin15 <ajdins@uia.no>
2025-03-18 12:49:25 +0100

    Wip: khn

--- ciem_nodnettapplikasjon.client/src/App.jsx
+++ ciem_nodnettapplikasjon.client/src/App.jsx
@@ -3,9 +3,7 @@ import { HashRouter as Router, Routes, Route, useLocation, Navigate } from 'reac
 import Login from './pages/Login/Login.jsx';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
-import KHSnettverk from './pages/KHS/KHSnettverk.jsx';
-import { HashRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
-import KHN from './pages/KHN/KHNnettverk.jsx';
+import KHSnettverk from './pages/KHN/KHNnettverk.jsx';
 import Actors from './pages/Actors/Actors.jsx';
 import ActorsList from './components/ActorsList/ActorsList.jsx';
 import ActorInfo from './pages/Actors/ActorInfo.jsx';
@@ -14,32 +12,26 @@ import VolActors from './pages/Actors/VolActors.jsx';
 import AllActors from './pages/Actors/AllActors.jsx';
 import PrivateActors from './pages/Actors/PrivateActors.jsx';
 
-
 function Layout() {
   const location = useLocation();
   const hideNavBar = location.pathname === "/"; // Hide NavBar on Login Page
 
   return (
     <>
-      {!hideNavBar && <NavBar />} {/* Hide NavBar when on Login */}
+      {!hideNavBar && <NavBar />}
       <div className="pageContainer">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/krisehandterings-nettverk" element={<KHSnettverk />} /> {/* ✅ Fixed route */}
+          <Route path="/krisehandterings-nettverk" element={<KHSnettverk />} />
           <Route path="/actors" element={<Actors />} />
-          <Route path="*" element={<Navigate to="/dashboard" />} /> {/* Redirect invalid paths to Dashboard */}
-          <Route path="/" element={<Login/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/KHN" element={<KHN/>}/>
-          <Route path="/actors" element={<Actors/>}/> 
-          <Route path="/actorsList" element={<ActorsList/>}/>
-          <Route path="/actorInfo" element={<ActorInfo/>}/>
-          <Route path="/actorsGov" element={<GovActors/>}/>
-          <Route path="/actorsVol" element={<VolActors/>}/>
-          <Route path="/actorsAll" element={<AllActors/>}/>
-          <Route path="/actorsPrivate" element={<PrivateActors/>}/>
-          <Route path="*" element={<Navigate to="/dashboard"/>}/> {/* Sender brukeren til Dashboard ved ugyldig URL*/}
+          <Route path="/actorsList" element={<ActorsList />} />
+          <Route path="/actorInfo" element={<ActorInfo />} />
+          <Route path="/actorsGov" element={<GovActors />} />
+          <Route path="/actorsVol" element={<VolActors />} />
+          <Route path="/actorsAll" element={<AllActors />} />
+          <Route path="/actorsPrivate" element={<PrivateActors />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
         </Routes>
       </div>
     </>
