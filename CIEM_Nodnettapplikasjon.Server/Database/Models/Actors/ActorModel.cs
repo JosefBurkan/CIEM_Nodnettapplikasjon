9357330481b407943cff22966d0a56ad955281d7
Befalen <joakimnormann@yahoo.com>
2025-03-18 21:49:13 +0100

    filter funksjoner for kategori og type

--- CIEM_Nodnettapplikasjon.Server/Database/Models/Actors/ActorModel.cs
+++ CIEM_Nodnettapplikasjon.Server/Database/Models/Actors/ActorModel.cs
@@ -4,7 +4,7 @@ namespace CIEM_Nodnettapplikasjon.Server.Database.Models.Actors
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
+        public string Category { get; set; }
         public string ActorType { get; set; } // For filtrering - Humam or company actor
         public List<string>? SubActors { get; set; }
     }
